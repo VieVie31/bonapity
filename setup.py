58aaabdb924992804097b8164537748cb7a9e222
VieVie31 <orissermaroix@gmail.com>
2019-07-28 19:09:40 +0200

    Making a first version of bonapity...

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bonapity',
+    version='0.1',
+    description='The funniest joke in the world',
+    url='http://github.com/VieVie31/bonapity',
+    author='Olivier RISSER-MAROIX',
+    author_email='orissermaroix@gmail.com',
+    license='CC-BY',
+    packages=['bonapity'],
+    zip_safe=False
+)
