1d1fab5f99017bc7aed5beaff1cb95ef729b04b6
VieVie31 <orissermaroix@gmail.com>
2019-07-29 07:52:55 +0200

    Fixing description...

--- setup.py
+++ setup.py
@@ -3,7 +3,22 @@ from setuptools import setup
 setup(
     name='bonapity',
     version='0.1',
-    description='The funniest joke in the world',
+    description='Get a simple HTTP (GET) API with only this simple decorator : `@bonapity` !',
+    long_description="""
+        # 👀 BONAPITY 👀
+
+        `bonAPIty` is a python3 package allowing you to create simple API (GET) 
+        for your functions without writting any complicated line of server code
+        and it's even simpler than Flask !
+
+        You are the 👨‍🍳, just do what you do the best : cook code !
+        don't loose your time to 💁, we do it for you... :)
+
+        By type hinting your code we cast 
+        the received inputs to the right type ! 
+        so don't worry about it... 😀
+    """,
+    long_description_content_type='text/markdown',
     url='http://github.com/VieVie31/bonapity',
     author='Olivier RISSER-MAROIX',
     author_email='orissermaroix@gmail.com',
@@ -11,3 +26,4 @@ setup(
     packages=['bonapity'],
     zip_safe=False
 )
+
