5eaa524838521408a260144290d688e626cbc1b5
VieVie31 <orissermaroix@gmail.com>
2019-07-30 01:03:58 +0200

    New version... 0.1.5 :D

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(
     name='bonapity',
-    version='0.1.4',
+    version='0.1.5',
     description='Get a simple HTTP (GET) API with only this simple decorator : @bonapity !',
     long_description="""
 # 👀 BONAPITY 👀
 
-`bonAPIty` is a python3 package allowing you to create simple API (GET) 
-for your functions without writting any complicated line of server code
-and it's even simpler than Flask !
+`bonAPIty` is the simplest python3 package allowing you to create simple API 
+(GET & POST) for your functions without writting any complicated line of server code !
 
 You are the 👨‍🍳, just do what you do the best : cook code !
 don't loose your time to 💁, we do it for you... :)
