eac29b7d2d9464e7cca6f46128014bad055d0aef
VieVie31 <orissermaroix@gmail.com>
2019-07-31 08:52:05 +0200

    0.1.8 live

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bonapity',
-    version='0.1.7',
+    version='0.1.8',
     description='Get a simple HTTP (GET & POST) API with only this simple decorator : @bonapity !',
     long_description="""
 # 👀 BONAPITY 👀
