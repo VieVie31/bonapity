59661da5df48c55fb49dbf771de45a3c31b3b39d
VieVie31 <orissermaroix@gmail.com>
2019-08-04 00:57:58 +0200

    Removing pointless readme opening...

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup
 
 import bonapity
 
-with open('README.md') as readme:
-    setup(
-        name='bonapity',
-        version=bonapity.__version__,
-        description='Get a simple HTTP (REST) API with only this simple decorator : @bonapity !',
-        long_description='bonAPIty is a python3 package allowing you to create simple API (GET) for your functions without writing any complicated line of server code and it is even simpler than Flask !',
-        long_description_content_type='text/markdown',
-        url='https://vievie31.github.io/bonapity/',
-        author='Olivier RISSER-MAROIX',
-        author_email='orissermaroix@gmail.com',
-        license='CC-BY',
-        packages=['bonapity'],
-        zip_safe=False
-    )
+setup(
+    name='bonapity',
+    version=bonapity.__version__,
+    description='Get a simple HTTP (REST) API with only this simple decorator : @bonapity !',
+    long_description='bonAPIty is the simplest python3 package allowing you to create API for your functions without writing any complicated line of server code ! See more [here](https://vievie31.github.io/bonapity/) !',
+    long_description_content_type='text/markdown',
+    url='https://vievie31.github.io/bonapity/',
+    author='Olivier RISSER-MAROIX',
+    author_email='orissermaroix@gmail.com',
+    license='CC-BY',
+    packages=['bonapity'],
+    zip_safe=False
+)
 
 
