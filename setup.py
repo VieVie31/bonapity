4595c9e6803047d75e9f3a1f4731d80c50bd8918
VieVie31 <orissermaroix@gmail.com>
2019-08-04 23:29:53 +0200

    The package is designed for python >=3.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='bonapity',
     version=bonapity.__version__,
     description='Get a simple HTTP (REST) API with only this simple decorator : @bonapity !',
-    long_description='bonAPIty is the simplest python3 package allowing you to create API for your functions without writing any complicated line of server code ! See more [here](https://vievie31.github.io/bonapity/) !',
+    long_description='bonAPIty is the simplest python (3.7) package allowing you to create API for your functions without writing any complicated line of server code ! See more [here](https://vievie31.github.io/bonapity/) !',
     long_description_content_type='text/markdown',
     url='https://vievie31.github.io/bonapity/',
     author='Olivier RISSER-MAROIX',
