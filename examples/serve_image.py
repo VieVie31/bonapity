9ace527e6fd7296fe749f32deb153f5473c9f254
VieVie31 <orissermaroix@gmail.com>
2019-08-06 23:46:46 +0200

    auto-detection of mime-type...

--- examples/serve_image.py
+++ examples/serve_image.py
@@ -1,7 +1,7 @@
 from bonapity import bonapity
 
 
-@bonapity(name="image.png", custom_mine_type="image/png")
+@bonapity(name="image.png", mime_type="auto")
 def myimage():
     return open("serve_image.png", 'rb').read()
 
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     # As path of the image is relative you need to run this script from 
     # the directory containing this script and the image
     bonapity.serve()
-    # Go to `http://localhost:8888/image.png`
+    # Go to `http://localhost:8888/image.png` the loaded file
+    # And `http://localhost:8888/serve_image.png` the default static file
 
